190d4d1d985958cc7a76b98ffd4c6c11e6cb9665
Daniil K <danilkarachev234@mail.ru>
2026-03-01 01:40:19 +0500

    create CatalogPage

---
+++ client/src/pages/CatalogPage.tsx
@@ -0,0 +1,16 @@
+import Footer from "../components/layout/Footer";
+import Header from "../components/layout/Header";
+
+function CatalogPage() {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>CatalogPage</h1>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default CatalogPage;
