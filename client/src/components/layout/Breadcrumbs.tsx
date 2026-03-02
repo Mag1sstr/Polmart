5078246cf726ec05b5d7f595478170c83aaf3f9f
Daniil K <danilkarachev234@mail.ru>
2026-03-03 00:17:22 +0500

    update Breadcrumbs

--- client/src/components/layout/Breadcrumbs.tsx
+++ client/src/components/layout/Breadcrumbs.tsx
@@ -18,33 +18,37 @@ function Breadcrumbs() {
     <section className="py-2.5 bg-[#ecececb3]">
       <div className="container">
         <ul className="flex gap-2.5">
-          {path.map((str, i) => (
-            <Link key={str} to={`/${str}`}>
-              <li
-                className={clsx(
-                  "first-letter:uppercase flex gap-2.5 items-center underline cursor-pointer",
-                  str === path.at(-1) &&
-                    "text-(--prime) cursor-text no-underline!",
-                )}
-              >
-                {i > 0 && (
-                  <svg
-                    width="9"
-                    height="12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M1 1.427l5.5 4.878L1 11.183"
-                      stroke="#C4C4C4"
-                      strokeWidth="2"
-                    />
-                  </svg>
-                )}
-                {breadcrumbsNames[str] || str}
-              </li>
-            </Link>
-          ))}
+          {path.map((str, i) => {
+            const to = "/" + path.slice(0, i + 1).join("/");
+
+            return (
+              <Link key={str} to={to}>
+                <li
+                  className={clsx(
+                    "first-letter:uppercase flex gap-2.5 items-center underline cursor-pointer",
+                    str === path.at(-1) &&
+                      "text-(--prime) cursor-text no-underline!",
+                  )}
+                >
+                  {i > 0 && (
+                    <svg
+                      width="9"
+                      height="12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M1 1.427l5.5 4.878L1 11.183"
+                        stroke="#C4C4C4"
+                        strokeWidth="2"
+                      />
+                    </svg>
+                  )}
+                  {breadcrumbsNames[str] || str}
+                </li>
+              </Link>
+            );
+          })}
         </ul>
       </div>
     </section>
